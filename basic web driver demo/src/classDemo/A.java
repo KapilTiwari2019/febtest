36f7f3ed26ced3590296faa2b8b390da5ba8b71d
kapil <kapil@10.0.0.242>
2019-02-28 09:50:48 -0800

    AAA Java

---
+++ basic web driver demo/src/classDemo/A.java
@@ -0,0 +1,31 @@
+package classDemo;
+
+public class A {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Arathamatic operator");
+		
+		int a= 40;
+		int b= 20;
+		
+		int Result = a-b;
+		
+		System.out.println("Result = >> " + Result);
+		
+		System.out.println("Sample Multiply");
+		
+		int d= 15;
+		int e= 3;
+		
+		int Answer = d*e;
+				
+		System.out.println("Result = " +Answer);
+		
+	
+	}
+
+	
+}
+
