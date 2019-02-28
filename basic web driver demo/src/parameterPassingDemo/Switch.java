e9d32cb769a055fb5fe0b5ffecbbe427d4530bd2
kapil <kapil@10.0.0.242>
2019-02-28 09:20:23 -0800

    sharing switch sample

---
+++ basic web driver demo/src/parameterPassingDemo/Switch.java
@@ -0,0 +1,22 @@
+package parameterPassingDemo;
+
+public class Switch {
+	public static void main(String[] args) {
+		
+		int x = 100;
+		switch(80) {
+		case 90:
+			System.out.println("A+");
+			break;
+		case 80:
+			System.out.println("A");
+			break;
+		case 70:
+			System.out.println("B+");
+			break;
+			default:
+				System.out.println("All in B");
+		}
+	}
+
+}
